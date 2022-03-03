c1f25ca46b0f23d80ac2b768045c599c726fb3f9
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-03 09:15:13 +0530

    Update terraform-modules/terraform-databases/

--- redis/redis.tf
+++ redis/redis.tf
@@ -4,7 +4,7 @@ resource "aws_elasticache_cluster" "redis" {
   node_type            = "cache.t3.micro"
   num_cache_nodes      = 1
   parameter_group_name = "default.redis6.2"
-  engine_version       = "6.2"
+  engine_version       = "6.x"
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.subnet-group.name
   security_group_ids   = []
