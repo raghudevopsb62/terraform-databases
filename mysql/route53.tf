efcedcc69a2029036cdaf21858151a0455bc1a2d
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-03 09:15:51 +0530

    Update terraform-modules/terraform-databases/

--- mysql/route53.tf
+++ mysql/route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "mysql" {
   zone_id = data.terraform_remote_state.vpc.outputs.INTERNAL_HOSTED_ZONEID
-  name    = "rabbitmq-${var.ENV}.${data.terraform_remote_state.vpc.outputs.INTERNAL_HOSTED_ZONE_NAME}"
+  name    = "mysql-${var.ENV}.${data.terraform_remote_state.vpc.outputs.INTERNAL_HOSTED_ZONE_NAME}"
   type    = "CNAME"
   ttl     = "300"
   records = [aws_db_instance.default.address]
