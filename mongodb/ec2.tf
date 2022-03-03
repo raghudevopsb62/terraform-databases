6ee8adf832125db8d9cd401e5185ec1548ba58d0
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-03 08:35:52 +0530

    Update terraform-modules/terraform-databases/

--- mongodb/ec2.tf
+++ mongodb/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_spot_instance_request" "spot-instance" {
   ami                    = data.aws_ami.ami.id
   instance_type          = var.MONGODB_INSTANCE_TYPE
   wait_for_fulfillment   = true
-  subnet_id              = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS, count.index)
+  subnet_id              = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNETS_IDS[0]
   vpc_security_group_ids = [aws_security_group.allow-mongodb.id]
 }
 
